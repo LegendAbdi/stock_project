a5767a559342d85083dddd86d3ae4f1de9a13ead
LegendAbdi <137934557+LegendAbdi@users.noreply.github.com>
2023-12-09 15:40:43 -0500

    Update product.js

--- product.js
+++ product.js
@@ -87,7 +87,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const PORT = process.env.port | 3000;
+const PORT = process.env.PORT | 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
